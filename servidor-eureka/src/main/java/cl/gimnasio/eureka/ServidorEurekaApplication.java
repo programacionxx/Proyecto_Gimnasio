f754c11179ed869e1a1716ec4e77d9b8d49de2c2
AV-Alumno <AV-Alumno@duoc.acad>
2026-05-13 12:36:02 -0400

    13/05

--- servidor-eureka/src/main/java/cl/gimnasio/eureka/ServidorEurekaApplication.java
+++ servidor-eureka/src/main/java/cl/gimnasio/eureka/ServidorEurekaApplication.java
@@ -3,6 +3,7 @@ package cl.gimnasio.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServidorEurekaApplication {
 
